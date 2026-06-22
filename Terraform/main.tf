9a471ec19f38f18e73c079cb7dbd8e98302e5d88
brad.toulson <brad.toulson@mitel.com>
2026-06-22 15:09:38 -0400

    Updates regarding resource group and TF version

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,20 +4,20 @@ resource "azurerm_resource_group" "crc_rg_prod" {
   location = var.location
 }
 
-# # Production storage account
-# resource "azurerm_storage_account" "crc_storage_prod" {
-#   name                     = "${var.unique_prefix}st${var.environment}"
-#   resource_group_name      = azurerm_resource_group.crc_rg_prod.name
-#   location                 = azurerm_resource_group.crc_rg_prod.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-#   account_kind             = "StorageV2"
+# Production storage account
+resource "azurerm_storage_account" "crc_storage_prod" {
+  name                     = "${var.unique_prefix}st${var.environment}"
+  resource_group_name      = azurerm_resource_group.crc_rg_prod.name
+  location                 = azurerm_resource_group.crc_rg_prod.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
 
-#   tags = {
-#     environment = var.environment
-#     project     = "cloud_resume_challenge"
-#   }
-# }
+  tags = {
+    environment = var.environment
+    project     = "cloud_resume_challenge"
+  }
+}
 
 # # Enable the static website engine
 # resource "azurerm_storage_account_static_website" "crc_static_site_prod" {
