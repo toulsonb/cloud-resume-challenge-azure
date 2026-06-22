4e78188f36bf9d2dcc1fc0d71c243655c766278c
brad.toulson <brad.toulson@mitel.com>
2026-06-22 14:53:00 -0400

    Naming cleanup

--- Terraform/api.tf
+++ Terraform/api.tf
@@ -6,8 +6,8 @@
 
 # resource "azurerm_cosmosdb_account" "crc_cosmos_prod" {
 #   name                = "${var.unique_prefix}-cosmos-${var.environment}"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.crc_rg_prod.location
+#   resource_group_name = azurerm_resource_group.crc_rg_prod.name
 #   offer_type          = "Standard"
 #   kind                = "GlobalDocumentDB"
 
@@ -16,20 +16,20 @@
 #   }
 
 #   geo_location {
-#     location          = azurerm_resource_group.rg.location
+#     location          = azurerm_resource_group.crc_rg_prod.location
 #     failover_priority = 0
 #   }
 # }
 
 # resource "azurerm_cosmosdb_sql_database" "crc_db_prod" {
 #   name                = "CloudResume" # Matches your target production database name
-#   resource_group_name = azurerm_resource_group.rg.name
+#   resource_group_name = azurerm_resource_group.crc_rg_prod.name
 #   account_name        = azurerm_cosmosdb_account.crc_cosmos_prod.name
 # }
 
 # resource "azurerm_cosmosdb_sql_container" "crc_container_prod" {
 #   name                = "Counter" # Matches your target container name
-#   resource_group_name = azurerm_resource_group.rg.name
+#   resource_group_name = azurerm_resource_group.crc_rg_prod.name
 #   account_name        = azurerm_cosmosdb_account.crc_cosmos_prod.name
 #   database_name       = azurerm_cosmosdb_sql_database.crc_db_prod.name
 #   partition_key_paths = ["/id"]
@@ -41,8 +41,8 @@
 # # Create the serverless Consumption plan (Y1 SKU)
 # resource "azurerm_service_plan" "crc_asp_prod" {
 #   name                = "${var.unique_prefix}-asp-${var.environment}"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.crc_rg_prod.location
+#   resource_group_name = azurerm_resource_group.crc_rg_prod.name
 #   os_type             = "Windows"
 #   sku_name            = "Y1"
 # }
@@ -50,11 +50,11 @@
 # # Create the Windows Function App for PowerShell
 # resource "azurerm_windows_function_app" "crc_function_prod" {
 #   name                = "func-${var.unique_prefix}-${var.environment}"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.crc_rg_prod.location
+#   resource_group_name = azurerm_resource_group.crc_rg_prod.name
 
-#   storage_account_name       = azurerm_storage_account.storage.name
-#   storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+#   storage_account_name       = azurerm_storage_account.crc_storage_prod.name
+#   storage_account_access_key = azurerm_storage_account.crc_storage_prod.primary_access_key
 #   service_plan_id            = azurerm_service_plan.crc_asp_prod.id
 
 #   site_config {
@@ -66,7 +66,7 @@
 #     cors {
 #       allowed_origins = [
 #         "https://bradtoulson.com",
-#         "https://${azurerm_storage_account.storage.name}.z13.web.core.windows.net"
+#         "https://${azurerm_storage_account.crc_storage_prod.name}.z13.web.core.windows.net"
 #       ]
 #     }
 #   }
